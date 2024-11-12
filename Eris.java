2f38c9cafd54545916e1b74eb441abb9429e02d9
Camydimario <166224855+Camydimario@users.noreply.github.com>
2024-11-12 14:47:19 -0300

    Update Eris.java

--- Eris.java
+++ Eris.java
@@ -1,4 +1,5 @@
 //implementacion del planeta Eris
+//Falta implementar la recompensa
 
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
@@ -8,23 +9,33 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class Eris extends ActorBase
+public class Eris extends ActorBase implements Dañable
 {
     /**
      * {@value #COMBUSTIBLE_CONTENIDO}
      */
     private static final int COMBUSTIBLE_CONTENIDO = 100;
-
+    protected static final int TAMAÑO_MINIMO_DE_ERIS = 10;
+    protected static final int TAMAÑO_MAXIMO_DE_ERIS = 100;
     private double ESCALA_X = 0.9;
     private double ESCALA_Y = 0.9;
     protected int vida;
+    protected int tamaño;
+    private int combustible;
+    private int MAX_COMBUSTIBLE = 100;
 
     public Eris() {
-        this.vida = 51 + (int) (Math.random() * 50);
+        this.vida = 100 + (int) (Math.random() * 50);
+        this.combustible = 0;
     }
 
-    public Eris(int vida) {
-        this.vida = vida;
+    /**
+     * Inicializa un Asteroiode con tamaño arbitrario, en el rango de 10 a
+     * {@value #TAMAÑO_MAXIMO_DE_ASTEROIDE} puntos
+     * 
+     * @param tamañoInicial
+     */public  Eris(int tamañoInicial) {
+        this.tamaño = Math.max(TAMAÑO_MINIMO_DE_ERIS , Math.min(TAMAÑO_MAXIMO_DE_ERIS , tamañoInicial));
     }
 
     /**
@@ -87,8 +98,51 @@ public class Eris extends ActorBase
         actualizarImagen();
     }
 
+    /**
+     * {@value #TAMAÑO_MAXIMO_DE_ASTEROIDE}
+     * 
+     * @return el tamaño máximo que puede tener un Asteroide
+     */
     protected int obtenerTamañoMaximo() {
-        return 100;
+        return TAMAÑO_MAXIMO_DE_ERIS ;
+    }
+
+    /**
+     * {@link #tamaño}
+     * 
+     * @return el tamaño del Asteroide
+     */
+    public int obtenerTamaño() {
+        return this.tamaño;
+    }
+
+    // Método que se llama cuando el planeta es destruido
+    public void serDestruidoPor(Atacante atacante) {
+        // Devuelve 100% de combustible a la nave
+        recibirCombustible(100);
+
+        // Elimina el planeta del mundo
+        getWorld().removeObject(this);
+    }
+
+    public void recibirCombustible(int cantidad) {
+        this.combustible += cantidad;
+        // Asegúrate de no exceder un límite máximo si es necesario
+        this.combustible = Math.min(this.combustible, MAX_COMBUSTIBLE);
+    }
+//para devoolver combustible aun no funciona
+    int recolectar(int cantidad) {
+        cantidad = Math.min(cantidad, this.vida);
+        cantidad = Math.min(cantidad, 100);
+        this.vida -= cantidad;
+
+        actualizarImagen();
+        explotar();
+        if (this.vida <= 0) {
+            getWorld().removeObject(this);
+        }
+
+        return cantidad;
     }
 
 }
